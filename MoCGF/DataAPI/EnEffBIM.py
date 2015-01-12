dadc2fe033f08089cbcbea98ccf59533dba6b853
jraedler <joerg@j-raedler.de>
2015-01-12 16:56:00 +0100

    MoCGF initial commit
    (no gbXML)
    for #5

---
+++ MoCGF/DataAPI/EnEffBIM.py
@@ -0,0 +1,19 @@
+#-*- coding:utf-8 -*-
+#
+# This file is part of MoCGF - a code generation framework
+# 20141114 Joerg Raedler jraedler@udk-berlin.de
+#
+
+name = 'EnEffBIM'
+description = 'Data API for MoCGF to load SimModel/EnEff-BIM data.'
+version = '0.1'
+author = 'EnEff-BIM team'
+helptxt = """
+This is the help text for EnEffBIM.
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+"""
+
+
+def fetchData(uriList=[]):
+    # FIXME: add Yuns API here
+    return {'foo' : 42}
---
+++ MoCGF/DataAPI/__init__.py
@@ -0,0 +1,11 @@
+#-*- coding:utf-8 -*-
+#
+# This file is part of MoCGF - a code generation framework
+# 20141114 Joerg Raedler jraedler@udk-berlin.de
+#
+
+# List all APIs here as subpackages 
+# specify the name of the MODULE (filename without .py)
+# the API name (name = ...) may be different!
+
+__all__ = ['Dummy', 'EnEffBIM']
