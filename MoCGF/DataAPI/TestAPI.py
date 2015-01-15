3524e6af052ede233bc94313057c8ceab79976b6
PRemmen <Premmen@eonerc.rwth-aachen.de>
2015-01-15 14:27:57 +0100

    first draft of TestGenerator

--- MoCGF/DataAPI/TestAPI.py
+++ MoCGF/DataAPI/TestAPI.py
@@ -15,11 +15,11 @@ def fetchData(uriList=[]):
     class Project(object):
     
     
-        def __init__(self,model):
+        def __init__(self):
             self.projectName = 'TestModel'
             self.modelDescription = 'This is just a Testmodel'
             self.libraryName = 'AixLib'
-            self.model = model
+            #self.model = model
         
     class Model(object):
         
@@ -27,17 +27,24 @@ def fetchData(uriList=[]):
             
             self.modelName = ''
             
+    class Connections(object):
+        
+        def __init__(self):
+            
+            self.connections = {}
+            
     boiler = Model()
     pump = Model()
-    
-    boiler.modelName = 'AixLib.HVAC.HeatGeneration.Boiler'
-    boiler.maxPower = 1000.0
+    connections = Connections()
     
     
-    pump.modelName = 'AixLib.HVAC.Pumps.Pump'
-    
-    print(boiler.__class__.__dict__)
-    
-    
-    
-    return {'project' : Project([boiler,pump])}
+    boiler.modelName= 'Boiler'
+    boiler.modelPath= 'HVAC/HeatGeneration/Boiler'
+    boiler.power = 1000.0
+    pump.modelName= 'Pump'
+    pump.modelPath= 'HVAC/HeatGeneration/Pump'
+    pump.head = 5.0
+    connections = Connections()
+    connections.connections = {'Boiler' : 'Pump', 'test1':'test2'}
+        
+    return {'project' : Project(), 'models' : [boiler,pump], 'connections' : connections}
