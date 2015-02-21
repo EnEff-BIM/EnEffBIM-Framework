ad95001968a48a9b910388e1206e0c144173392e
Joerg Raedler <joerg@j-raedler.de>
2015-02-21 11:03:46 +0100

    added code to apply python filter / preprocessor, see TestDummy for an example

--- MoCGF/import_file.py
+++ MoCGF/import_file.py
@@ -2,29 +2,43 @@
 
 import sys
 
-if sys.version_info >= (3, 4):
+if sys.version_info >= (3, 3):
 
     import importlib
 
     def import_file(module_path='', module=''):
-        pass
+        importlib.invalidate_caches()
+        if module in sys.modules:
+            del sys.modules[module]
+        sys.path.insert(0, module_path)
+        loader = importlib.find_loader(module)
+        del sys.path[0]
+        m = loader.load_module(module)
+        return m
 
-elif sys.version_info >= (3, 3):
-
-    import importlib
-
-    def import_file(module_path='', module=''):
-        pass
 
 elif sys.version_info >= (2, 7):
 
     def import_file(module_path='', module=''):
-        pass
+        if module in sys.modules:
+            del sys.modules[module]
+        sys.path.insert(0, module_path)
+        m = __import__(module)
+        del sys.path[0]
+        return m
 
 else:
     raise NotImplementedError('This modules functions are not implemented for python <2.7')
 
 
 if __name__ == '__main__':
-    # test code
-    pass
+    # test this module with path and module name as arguments
+    # will print modules namespace (without __builtins__)
+
+    import pprint
+    
+    p, n = sys.argv[1:]
+    m = import_file(p, n)
+    d = m.__dict__
+    del d['__builtins__']
+    pprint.pprint(d)
