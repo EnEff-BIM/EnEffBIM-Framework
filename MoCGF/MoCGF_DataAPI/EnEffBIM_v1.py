a469e35952983df545d5f611e8710cb1e81b3d85
thorade <matthis.thorade@gmail.com>
2015-07-09 15:08:48 +0200

    fetchData

--- MoCGF/MoCGF_DataAPI/EnEffBIM_v1.py
+++ MoCGF/MoCGF_DataAPI/EnEffBIM_v1.py
@@ -11,28 +11,30 @@ helptxt = """
 Trying to use the API
 """
 
-import os
-import sys
+def fetchData(uriList, systemCfg, generatorCfg, logger):
+    import os
+    import sys
 
-rootPath = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-modulePath = os.path.join(rootPath, 'Generic_API\Generic_API_1.0')
-dllPath = os.path.join(rootPath, 'Generic_API\Generic_API_1.0\PythonAPI')
-useCaseLoc = os.path.join(rootPath, "Generic_API\\Generic_API_1.0\\xml_use_case\\Boiler_Gas_VDI6020_Test.xml")
-mapRuleLoc = os.path.join(rootPath, "Generic_API\\Generic_API_1.0\\xml_mapping_rule\\AixLib_Mapping_Rule.xml")
+    rootPath = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    modulePath = os.path.join(rootPath, 'Generic_API\Generic_API_1.0')
+    dllPath = os.path.join(rootPath, 'Generic_API\Generic_API_1.0\PythonAPI')
+    useCaseLoc = os.path.join(rootPath, "Generic_API\\Generic_API_1.0\\xml_use_case\\Boiler_Gas_VDI6020_Test.xml")
+    mapRuleLoc = os.path.join(rootPath, "Generic_API\\Generic_API_1.0\\xml_mapping_rule\\AixLib_Mapping_Rule.xml")
 
-# add dllPath to Windows Path
-os.environ['PATH'] = ';'.join([dllPath, os.environ['PATH']])
-# add modulePath to Python Path
-sys.path.append(modulePath)
+    # add dllPath to Windows Path
+    os.environ['PATH'] = ';'.join([dllPath, os.environ['PATH']])
+    # add modulePath to Python Path
+    sys.path.append(modulePath)
 
-from PythonAPI import simmodel2modelica as lsm
+    from PythonAPI import simmodel2modelica as lsm
+    # create mapped data object
+    MapData = lsm.RuleData()
+    # set data location
+    MapData.setDataLocation(useCaseLoc, mapRuleLoc)
+    # transform SimModel data into Modelica objects
+    MapData.transformModel()
 
-# create mapped data object
-MapData = lsm.RuleData()
-# set data location
-MapData.setDataLocation(useCaseLoc, mapRuleLoc)
-# transform SimModel data into Modelica objects
-MapData.transformModel()
+    simProject = MapData.getSimProject()
+    location = simProject.getWeatherLocationCity()
 
-simProject = MapData.getSimProject()
-print("Weather location city: " + simProject.getWeatherLocationCity())
+    return {location}
