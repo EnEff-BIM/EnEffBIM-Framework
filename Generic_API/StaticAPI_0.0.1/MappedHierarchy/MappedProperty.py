6cddb86855b4a76f7bee9b1c81007f5bfdde22dd
PRemmen <Premmen@eonerc.rwth-aachen.de>
2015-09-24 16:47:43 +0200

    Add MappedProperty content, #68

--- Generic_API/StaticAPI_0.0.1/MappedHierarchy/MappedProperty.py
+++ Generic_API/StaticAPI_0.0.1/MappedHierarchy/MappedProperty.py
@@ -13,9 +13,17 @@ class MappedProperty(object):
     Attributes
     ----------
     
-   
+    name : str
+        Modelica name of the Parameter (e.g. Q_flow_max)
+        
+    value : float/int/bool/str/list
+        Value of the Property, can be float, int, boolean, string or a list 
+        with corresponding data type.
         
     
+        
+    
+
     """
     
     def __init__(self, parent_component):
