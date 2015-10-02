58f5ed198c77747a2e8b591d0dcd09317772fc43
PRemmen <Premmen@eonerc.rwth-aachen.de>
2015-10-02 12:10:40 +0200

    New StaticAPI UseCase, change pump, now we have records within records. Adapt Template slightly, #68

--- Generic_API/StaticAPI_0.0.1/MappedHierarchy/FilledMapped.py
+++ Generic_API/StaticAPI_0.0.1/MappedHierarchy/FilledMapped.py
@@ -7,144 +7,130 @@ from MappedHierarchy.MappedRecord import MappedRecord
 from MappedHierarchy.MappedConnector import MappedConnector
 from MappedHierarchy.MappedConnection import MappedConnection
 
-def return_mapped_project():
-    m_prj = MappedProject()
+'''
+StaticAPI Use Case Scenario:
+
+1 Building
+    one boiler
+        control: constant flow temperature
+    one pump
+        control: prescribed pressure rise
+    2 connecting pipes
+2 Thermal Zones
+    each one Radiator
+        one valve
+            control room set temperature P Controlled
+'''
     
+
+
+def return_mapped_project():
+    '''Instantiate MappedPropject, fill attributes'''
+    m_prj = MappedProject()
     m_prj.used_library = "AixLib"
     m_prj.library_version = "0.1.0"
     m_prj.project_name = "StaticAPI"
     
+    '''Instantiate MappedBuilding, fill attributes, add building to project'''
     m_bldg = MappedBuilding(m_prj)
-            
     m_bldg.building_name = "UseCase1.1"
+    m_prj.buildings = [m_bldg]
+    '''Instantiate MappedThermalZone m_tz1, m_tz2, fill attributes, add thermal zone to building'''
+    m_tz1 = MappedThermalZone(m_bldg)
+    m_tz1.zone_name = "OneZone"
+    tz1_conv = add_mapped_connector(m_tz1, "internalGainsCon", "HeatPort")
+    tz1_rad = add_mapped_connector(m_tz1, "internalGainsRad", "Star")
     
-    m_prj.connections_hard = [["weather.WeatherDataVector", "thermalZone.weather"],
-                              ["weather.SolarRadiation_OrientedSurfaces", "thermalZone.solarRad_in"],
-                              ["infiltrationRate.y", "thermalZone.infiltrationRate"],
-                              ["internalLoads.y", "thermalZone.internalGains"],
-                              ["pump.port_b", "boiler.port_a"],
-                              ["boiler.port_b", "flowPipe.port_a"],
-                              ["valve.port_a", "flowPipe.port_b"],
-                              ["valve.port_b", "radiator.port_a"],
-                              ["radiator.port_b", "returnPipe.port_a"],
-                              ["pump.port_a", "returnPipe.port_b"],
-                              ["radiator.convPort", "thermalZone.internalGainsConv"],
-                              ["thermalZone.internalGainsConv", "temperatureSensor.port"],
-                              ["temperatureSensor.T", "PID.u_m"],
-                              ["PID.u_s", "setTemp.y"],
-                              ["PID.y", "valve.opening"],
-                              ["nightSignal.y", "pump.IsNight"],
-                              ["thermalZone.internalGainsRad", "radiator.radPort"],
-                              ["flowTemp.y", "boiler.T_set"],
-                              ["weather.WeatherDataVector[1]", "thermalZone.infiltrationTemperature"],
-                              ["expansionVessel.ports[1]", "pump.port_a"]]
-
-    #Pipe 1
-    m_bldg_sc_pipe1 = return_mapped_component(m_bldg,\
-                             "AixLib.Fluid.FixedResistances.StaticPipe", \
-                             "flowPipe")
+    m_tz2 = MappedThermalZone(m_bldg)
+    m_tz2.zone_name = "TwoZone"
+    tz2_conv = add_mapped_connector(m_tz2, "internalGainsCon", "HeatPort")
+    tz2_rad = add_mapped_connector(m_tz2, "internalGainsRad", "Star")
     
-    add_mapped_property(m_bldg_sc_pipe1, "Medium",\
-                             "Modelica.Media.Water.ConstantPropertyLiquidWater")
+    m_bldg.thermal_zones = [m_tz1, m_tz2]
+    
+    '''Instantiate Boiler as MappedComponent, add Properties, add connectors'''
+    
+    m_bldg_sc_boiler = return_mapped_component(m_bldg,"AixLib.Fluid.HeatExchangers.Boiler","boiler")
+    add_mapped_property(m_bldg_sc_boiler, "Medium", "Modelica.Media.Water.ConstantPropertyLiquidWater")  
+    add_mapped_property(m_bldg_sc_boiler,"Q_flow_max", 1000.0)
+    add_mapped_property(m_bldg_sc_boiler, "Volume", 0.102)
+    add_mapped_record(m_bldg_sc_boiler, "boilerEfficiencyB", "AixLib.DataBase.Boiler")
+    add_mapped_property(m_bldg_sc_boiler.parameters[3], "boilerEfficiency", [[0,0.5,1],[0.8,0.9,1.1]])
+    boiler_a = add_mapped_connector(m_bldg_sc_boiler, "port_a", "Fluid")
+    boiler_b = add_mapped_connector(m_bldg_sc_boiler, "port_b", "Fluid")
+    boiler_real = add_mapped_connector(m_bldg_sc_boiler, "T_set", "Real")
+    
+    '''Instantiate Pump as MappedComponent, add Properties, add connectors'''
+    
+    m_bldg_sc_pump = return_mapped_component(m_bldg,"AixLib.Fluid.Movers.FlowControlled_dp","pump")
+    add_mapped_property(m_bldg_sc_pump, "Medium","Modelica.Media.Water.ConstantPropertyLiquidWater")
+    add_mapped_property(m_bldg_sc_pump, "addPowerToMedium", False)
+    add_mapped_record(m_bldg_sc_pump, "per", "AixLib.Fluid.Movers.Data.FlowControlled")
+    add_mapped_record(m_bldg_sc_pump.parameters[2], "hydraulicEfficiency", "AixLib.Fluid.Movers.BaseClasses.Characteristics.efficiencyParameters")
+    add_mapped_property(m_bldg_sc_pump.parameters[2].parameters[0], "V_flow",[0,0.5,1])
+    add_mapped_property(m_bldg_sc_pump.parameters[2].parameters[0], "eta",[1,0.5,0])
+    pump_a = add_mapped_connector(m_bldg_sc_pump, "port_a", "Fluid")
+    pump_b = add_mapped_connector(m_bldg_sc_pump, "port_b", "Fluid")
+    pump_real = add_mapped_connector(m_bldg_sc_pump, "dp_in", "Real")
+    pump_bool = add_mapped_connector(m_bldg_sc_pump, "dp_in", "Real")
+    pump_heat = add_mapped_connector(m_bldg_sc_pump, "heatPort", "Heat")
+    
+    '''Instantiate Pipe pipe1 and pipe 2 as MappedComponent, add Properties, add connectors'''
+    
+    m_bldg_sc_pipe1 = return_mapped_component(m_bldg,"AixLib.Fluid.FixedResistances.StaticPipe", "flowPipe")
+    add_mapped_property(m_bldg_sc_pipe1, "Medium","Modelica.Media.Water.ConstantPropertyLiquidWater")
     add_mapped_property(m_bldg_sc_pipe1, "D", 0.01)
     add_mapped_property(m_bldg_sc_pipe1, "l", 10.01)
     add_mapped_property(m_bldg_sc_pipe1, "e", 0.000021)
-    pipe1_a = add_mapped_connector(m_bldg_sc_pipe1, "port_a", "Fluid", "Input")
-    pipe1_b = add_mapped_connector(m_bldg_sc_pipe1, "port_b", "Fluid", "Output")
+    pipe1_a = add_mapped_connector(m_bldg_sc_pipe1, "port_a", "Fluid")
+    pipe1_b = add_mapped_connector(m_bldg_sc_pipe1, "port_b", "Fluid")
     
-    
-    #Pipe 2
-    m_bldg_sc_pipe2 = return_mapped_component(m_bldg,\
-                             "AixLib.Fluid.FixedResistances.StaticPipe", \
-                             "flowPipe")
-    add_mapped_property(m_bldg_sc_pipe2, "Medium",\
-                             "Modelica.Media.Water.ConstantPropertyLiquidWater")
+    m_bldg_sc_pipe2 = return_mapped_component(m_bldg, "AixLib.Fluid.FixedResistances.StaticPipe","flowPipe")
+    add_mapped_property(m_bldg_sc_pipe2, "Medium","Modelica.Media.Water.ConstantPropertyLiquidWater")
     add_mapped_property(m_bldg_sc_pipe2, "D", 0.02)
     add_mapped_property(m_bldg_sc_pipe2, "l", 10.02)
     add_mapped_property(m_bldg_sc_pipe2, "e", 0.000022)
-    pipe2_a = add_mapped_connector(m_bldg_sc_pipe2, "port_a", "Fluid", "Input")
-    pipe2_b = add_mapped_connector(m_bldg_sc_pipe2, "port_b", "Fluid", "Output")
-    #Boiler
-    m_bldg_sc_boiler = return_mapped_component(m_bldg,\
-                             "AixLib.Fluid.HeatExchangers.Boiler", \
-                             "boiler")
-    add_mapped_property(m_bldg_sc_boiler, "Medium",\
-                             "Modelica.Media.Water.ConstantPropertyLiquidWater")
-    add_mapped_property(m_bldg_sc_boiler, "Q_flow_max", 1000.0)
-    add_mapped_property(m_bldg_sc_boiler, "Volume", 0.102)
-    add_mapped_record(m_bldg_sc_boiler, "boilerEfficiencyB", "AixLib.DataBase.Boiler")
-    add_mapped_property(m_bldg_sc_boiler.parameters[3], "boilerEfficiency",\
-                         [[0,0.5,1],[0.8,0.9,1.1]])
-    boiler_a = add_mapped_connector(m_bldg_sc_boiler, "port_a", "Fluid", "Input")
-    boiler_b = add_mapped_connector(m_bldg_sc_boiler, "port_b", "Fluid", "Output")
-    boiler_real = add_mapped_connector(m_bldg_sc_boiler, "T_set", "Real", "Input")
-    #Pump
-    m_bldg_sc_pump = return_mapped_component(m_bldg,\
-                             "AixLib.Fluid.Movers.Pump", \
-                             "pump")
-    add_mapped_property(m_bldg_sc_pump, "Medium",\
-                             "Modelica.Media.Water.ConstantPropertyLiquidWater")
-    add_mapped_property(m_bldg_sc_pump, "ControlStrategy", 0)
-    add_mapped_property(m_bldg_sc_pump, "Head_max", 5)
-    add_mapped_property(m_bldg_sc_pump, "V_flow_max", 2)
-    add_mapped_record(m_bldg_sc_pump, "MinMaxCharacteristics", "AixLib.DataBase.Pump")
-    add_mapped_property(m_bldg_sc_pump.parameters[4], "minMaxHead",\
-                         [[0,0.5,1],[0.8,0.9,1.1],[0.1,0.2,0.3]])
-    m_bldg.supply_components = [m_bldg_sc_pipe1, m_bldg_sc_pipe2, m_bldg_sc_boiler, m_bldg_sc_pump]
-    pump_a = add_mapped_connector(m_bldg_sc_pump, "port_a", "Fluid", "Input")
-    pump_b = add_mapped_connector(m_bldg_sc_pump, "port_b", "Fluid", "Output")
-    pump_bool = add_mapped_connector(m_bldg_sc_pump, "IsNight", "Boolean", "Input")
+    pipe2_a = add_mapped_connector(m_bldg_sc_pipe2, "port_a", "Fluid")
+    pipe2_b = add_mapped_connector(m_bldg_sc_pipe2, "port_b", "Fluid")
+     
+    '''Add Pipe1, Pipe2, Boiler, Pump to the buildings components'''
     
-    #first zone  
-    m_tz1 = MappedThermalZone(m_bldg)
-    m_tz1.zone_name = "OneZone"
-    m_tz1_sc_radiator = return_mapped_component(m_tz1, \
-                            "AixLib.Fluid.HeatExchangers.Radiators.Radiator", \
-                            "radiator_tz1")
-    add_mapped_property(m_tz1_sc_radiator, "Medium",\
-                             "Modelica.Media.Water.ConstantPropertyLiquidWater")
-    add_mapped_record(m_tz1_sc_radiator, "RadiatorType", \
-                             "AixLib.DataBase.Radiators")
+    m_bldg.supply_components = [m_bldg_sc_pipe1, m_bldg_sc_pipe2, m_bldg_sc_boiler, m_bldg_sc_pump]
+   
+  
+    '''Instantiate Radiator 1 and 2 as MappedComponent, add Properties, add connectors, add to thermal zone'''
+    
+    m_tz1_sc_radiator = return_mapped_component(m_tz1,"AixLib.Fluid.HeatExchangers.Radiators.Radiator","radiator_tz1")
+    add_mapped_property(m_tz1_sc_radiator, "Medium","Modelica.Media.Water.ConstantPropertyLiquidWater")
+    add_mapped_record(m_tz1_sc_radiator, "RadiatorType","AixLib.DataBase.Radiators")
+    
     add_mapped_property(m_tz1_sc_radiator.parameters[1], "NominalPower", 885.94)
     add_mapped_property(m_tz1_sc_radiator.parameters[1], "T_flow_nom", 56.94)
     add_mapped_property(m_tz1_sc_radiator.parameters[1], "T_return_nom", 36.94)
     
-    rad1_a = add_mapped_connector(m_tz1_sc_radiator, "port_a", "Fluid", "Input")
-    rad1_b = add_mapped_connector(m_tz1_sc_radiator, "port_b", "Fluid", "Output")
-    rad1_conv = add_mapped_connector(m_tz1_sc_radiator, "convPort", "HeatPort", "Output")
-    rad1_rad = add_mapped_connector(m_tz1_sc_radiator, "radPort", "Star", "Output")
-    
-    tz1_conv = add_mapped_connector(m_tz1, "internalGainsCon", "HeatPort", "Input")
-    tz1_rad = add_mapped_connector(m_tz1, "internalGainsRad", "Star", "Input")
-    
+    rad1_a = add_mapped_connector(m_tz1_sc_radiator, "port_a", "Fluid")
+    rad1_b = add_mapped_connector(m_tz1_sc_radiator, "port_b", "Fluid")
+    rad1_conv = add_mapped_connector(m_tz1_sc_radiator, "convPort", "HeatPort")
+    rad1_rad = add_mapped_connector(m_tz1_sc_radiator, "radPort", "Star")
+       
     m_tz1.supply_components = [m_tz1_sc_radiator]
-    #seccond zone 
-    m_tz2 = MappedThermalZone(m_bldg)
-    m_tz2.zone_name = "TwoZone"
-    m_tz2_sc_radiator = return_mapped_component(m_tz2, \
-                            "AixLib.Fluid.HeatExchangers.Radiators.Radiator", \
-                            "radiator_tz2")
-    add_mapped_property(m_tz2_sc_radiator, "Medium",\
-                             "Modelica.Media.Water.ConstantPropertyLiquidWater")
-    add_mapped_record(m_tz2_sc_radiator, "RadiatorType", \
-                             "AixLib.DataBase.Radiators")
+
+    m_tz2_sc_radiator = return_mapped_component(m_tz2,"AixLib.Fluid.HeatExchangers.Radiators.Radiator","radiator_tz2")
+    add_mapped_property(m_tz2_sc_radiator, "Medium","Modelica.Media.Water.ConstantPropertyLiquidWater")
+    add_mapped_record(m_tz2_sc_radiator, "RadiatorType","AixLib.DataBase.Radiators")
+    
     add_mapped_property(m_tz2_sc_radiator.parameters[1], "NominalPower", 885.94)
     add_mapped_property(m_tz2_sc_radiator.parameters[1], "T_flow_nom", 56.94)
     add_mapped_property(m_tz2_sc_radiator.parameters[1], "T_return_nom", 36.94)
     
-    rad2_a = add_mapped_connector(m_tz2_sc_radiator, "port_a", "Fluid", "Input")
-    rad2_b = add_mapped_connector(m_tz2_sc_radiator, "port_b", "Fluid", "Output")
-    rad2_conv = add_mapped_connector(m_tz2_sc_radiator, "convPort", "HeatPort", "Output")
-    rad2_rad = add_mapped_connector(m_tz2_sc_radiator, "radPort", "Star", "Output")
-    
-    tz2_conv = add_mapped_connector(m_tz2, "internalGainsCon", "HeatPort", "Input")
-    tz2_rad = add_mapped_connector(m_tz2, "internalGainsRad", "Star", "Input")
-    
+    rad2_a = add_mapped_connector(m_tz2_sc_radiator, "port_a", "Fluid")
+    rad2_b = add_mapped_connector(m_tz2_sc_radiator, "port_b", "Fluid")
+    rad2_conv = add_mapped_connector(m_tz2_sc_radiator, "convPort", "HeatPort")
+    rad2_rad = add_mapped_connector(m_tz2_sc_radiator, "radPort", "Star") 
     
     m_tz2.supply_components = [m_tz2_sc_radiator]
-    m_bldg.thermal_zones = [m_tz1, m_tz2]
-    m_prj.buildings = [m_bldg]
     
+    '''Add all connections'''
     #FluidConnections
     add_mapped_connections(m_prj, pipe1_b, rad1_a , "Fluid")
     add_mapped_connections(m_prj, rad1_b, rad2_a, "Fluid")
@@ -157,9 +143,6 @@ def return_mapped_project():
     add_mapped_connections(m_prj, rad2_rad, tz2_rad, "Star")
     add_mapped_connections(m_prj, rad1_conv, tz1_conv, "Heat")
     add_mapped_connections(m_prj, rad1_rad, tz1_rad, "Star")
-    #
-    
-    
     
     return m_prj
     
@@ -188,7 +171,7 @@ def return_mapped_component(parent, target_location, target_name):
     
     return mapped_comp
 
-def add_mapped_connector(parent, name, type, input_output):
+def add_mapped_connector(parent, name, type):
     
     mapped_con = MappedConnector(parent)
     mapped_con.name = name
