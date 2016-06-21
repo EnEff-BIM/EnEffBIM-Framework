0095967d985c5047bd1b080983cef975838fed79
PRemmen <PRemmen@eonerc.rwth-aachen.de>
2016-06-21 11:52:41 +0200

    Add mapping rules

--- MapAPI/mapapi/molibs/TopologyMapping.py
+++ MapAPI/mapapi/molibs/TopologyMapping.py
@@ -13,13 +13,14 @@ rootPath = os.path.dirname(__file__)
 default_path = rootPath[:rootPath.rfind("EnEffBIM-Framework")]
 modulePath = os.path.join(default_path, "EnEffBIM-Framework\\SimModel_Python_API\\simmodel_swig\\Release")
 
-
+print(modulePath)
 from mapapi.MapClasses import MapProject
 
 zoneFile_path = os.path.join(modulePath+"\\SingleZoneWithInternalLoads.simxml")
 hvacFile_path = os.path.join(modulePath+"\\1.1BoilerGasRadiator.simxml")
 fullFile_path = os.path.join(modulePath+"\\Boiler_Gas_VDI6020_V12.simxml")
 pathList = (zoneFile_path, hvacFile_path)
+mappingFile_path = os.path.join(modulePath+"\\mapping_rule\\mapping_rule_xml\\AixLib.xml")
 
-prj = MapProject(fullFile_path)
+prj = MapProject(fullFile_path, mappingFile_path)
 print("finish")
