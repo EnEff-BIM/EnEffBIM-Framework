838cb236a47b171eae527e1ba022d4ef49c86c7f
PRemmen <PRemmen@eonerc.rwth-aachen.de>
2016-07-20 22:28:41 +0200

    test update mapping rules

--- MapAPI/mapapi/molibs/AixLib/Fluid/FixedResitances/StaticPipe.py
+++ MapAPI/mapapi/molibs/AixLib/Fluid/FixedResitances/StaticPipe.py
@@ -24,11 +24,26 @@ class Pipe(MapHierarchy.MapComponent):
             self.target_name = "Pipe"+str(random.randint(0,10))
         else:
             pass
-        self.target_location = "AixLib.HVAC.Pipes.StaticPipe"
-        self.D = self.add_parameter(name="D",
+
+        if self.hierarchy_node is None:
+            self.target_location = "AixLib.Fluid.FixedResistances.StaticPipe"
+            self.D = self.add_parameter(name="D",
                                     value=0.05)
-        self.l = self.add_parameter(name="l",
-                                    value=1)
-        self.e = self.add_parameter(name="e",
-                                    value=0.000025)
+            self.l = self.add_parameter(name="l",
+                                        value=1)
+            self.e = self.add_parameter(name="e",
+                                        value=0.000025)
+        else:
+            self.target_location = "AixLib.Fluid.FixedResistances.StaticPipe"
+            self.D = self.add_parameter(name="D",
+                                    value=0.05)
+            self.l = self.add_parameter(name="l",
+                                        value=1)
+            self.e = self.add_parameter(name="e",
+                                        value=0.000025)
+            #map_sim = self.hierarchy_node.getMappedComponents()
+            #self.target_location = map_sim[0].getTargetLocation()
+            #prop_list = map_sim[0].getMappedPropertyList()
+            #self.arrange_parameters(prop_list)
+
         return True
