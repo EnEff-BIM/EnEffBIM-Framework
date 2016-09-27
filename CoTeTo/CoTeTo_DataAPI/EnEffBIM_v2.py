e8f785c85a6d12a8f1aedc1a9521079af144612c
PRemmen <PRemmen@eonerc.rwth-aachen.de>
2016-09-27 18:01:16 +0200

    usage of API with CoTeTo

--- CoTeTo/CoTeTo_DataAPI/EnEffBIM_v2.py
+++ CoTeTo/CoTeTo_DataAPI/EnEffBIM_v2.py
@@ -18,18 +18,19 @@ def fetchData(uriList, systemCfg, generatorCfg, logger):
     modulePath = os.path.join(default_path, "EnEffBIM-Framework\\SimModel_Python_API"
                               "\\simmodel_swig\\Release")
     print(modulePath)
-    zoneFile_path = os.path.join(modulePath+"\\SingleZoneWithInternalLoads.simxml")
-    hvacFile_path = os.path.join(modulePath+"\\1.1_Architecture+HVAC+Zone_Curve+Schedule_korr_ZumTesten.simxml")
+    zoneFile_path = os.path.join(modulePath,
+                                 "UseCase1_1_BoilerGasRadiatorFromSimergy.simxml")
+    hvacFile_path = os.path.join(modulePath,
+                                 "1.1_Architecture+HVAC+Zone_Curve+Schedule_korr.simxml")
     fullFile_path = os.path.join(modulePath,"Boiler_Gas_VDI6020_V12.simxml")
     pathList = (zoneFile_path, hvacFile_path)
     print(pathList[0])
-    mapping_file = os.path.join(modulePath,
-                                "mapping_rule\\mapping_rule_xml\\AixLib.xml")
+    mappingFile_path = os.path.join(
+        modulePath + "\\mapping_rule\\mapping_rule_xml\\AixLib_v2.0.xml")
     from mapapi.MapClasses import MapProject
     prj = MapProject(pathList,
-                     mapping_file)
+                     mappingFile_path)
       
     dataDictionary=dict(
-        MapData = prj
-    )
+        MapData = prj )
     return dataDictionary
