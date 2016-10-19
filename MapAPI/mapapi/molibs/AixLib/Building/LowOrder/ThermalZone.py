631c99a8ab0660c2398f8fe8a4e476d281137acd
PRemmen <PRemmen@eonerc.rwth-aachen.de>
2016-10-19 17:10:46 +0200

    typo ventilation rate

--- MapAPI/mapapi/molibs/AixLib/Building/LowOrder/ThermalZone.py
+++ MapAPI/mapapi/molibs/AixLib/Building/LowOrder/ThermalZone.py
@@ -19,8 +19,8 @@ class ThermalZone(MapHierarchy.MapThermalZone):
         self.infiltrationTemperature = self.add_connector(
                                         name="infiltrationTemperature",
                                         type="Real")
-        self.infiltrationRate = self.add_connector(name="infiltrationRate",
-                                                   type="Real")
+        self.ventilationRate = self.add_connector(name="ventilationRate ",
+                                                  type="Real")
         self.internalGains = self.add_connector(name="internalGains",
                                                 type="Real",
                                                 dimension=3)
@@ -56,7 +56,7 @@ class ThermalZone(MapHierarchy.MapThermalZone):
         const.init_me()
         const.target_name = "infil" + "_" + self.target_name
         const.add_parameter('k', rate)
-        const.add_connection(const.y, self.infiltrationRate)
+        const.add_connection(const.y, self.ventilationRate)
         self.project.mod_components.append(const)
 
     def internal_loads(self):
