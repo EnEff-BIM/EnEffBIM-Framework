5f5de0deedd403873cb4ff2587e70658a5f9ad84
Carles Ribas Tugores <c.ribastugores@udk-berlin.de>
2016-10-21 16:07:44 +0200

    update - create graphml of the HVAC loop.

--- SimModel_Python_API/simmodel_swig/Release/ObtainPipeInfo.py
+++ SimModel_Python_API/simmodel_swig/Release/ObtainPipeInfo.py
@@ -1,5 +1,5 @@
 import os
-
+import networkx as nx
 # load SimModel hierarchy
 import SimModel_Hierachy
 # load SimModel mapped data
@@ -101,23 +101,108 @@ mappedComonentList = simxml_mapped_data.getMappedComponentList()
 sim_hierarchy = translator.getSimHierarchy()
 nodeList = sim_hierarchy.getHierarchyNodeList()
    
-# iterate each hiearchy node saved in the list
-for id in range(0, nodeList.size()):
-    print("node ", id, ":")
-    
-    # retrieve a node
+# iterate each hierarchy node saved in the list
+Structured_HVAC = []
+
+for id in range(0, nodeList.size()):    
     hierarchy_node = nodeList[id]
-    
-    # check the class type of the hiearchy node
-    if(hierarchy_node.isClassType("SimFlowSegment_Pipe_Indoor")):	
-        print("node class type: ", hierarchy_node.ClassType())
-        sim_object = hierarchy_node.getSimModelObject()
-        sim_object_id = sim_object.RefId()
-        child_node_list = hierarchy_node.getChildList()		
-        print("current SimModel object id: ", sim_object_id, " has the childs: ", child_node_list, "\n")
-        for x in child_node_list:
-            obj = x.getSimModelObject()		
-            child_node_2level = child_node_list[obj.RefId()]
-            print("current SimModel object id: ", sim_object_id, " has the childs: ", child_node_2level, "\n")			
-        
+    #print("node ", id, ":", hierarchy_node.getSimModelObject().RefId())    
+    # check the class type of the hierarchy node
+    if hierarchy_node.isClassType("SimSystem_HvacHotWater_Supply") or hierarchy_node.isClassType("SimSystem_HvacHotWater_Demand"):
+    # We are in Supply System: --> obtain child list.
+        print("Extract childs from ", hierarchy_node.getSimModelObject().RefId())	
+        child_node_list = hierarchy_node.getChildList()	
+        #print("child_node_list: ", child_node_list)
+        #sim_object = hierarchy_node.getSimModelObject()
+        #sim_object_id = sim_object.RefId()
+        #print("current SimModel object id: ", sim_object_id, " has the childs: ", child_node_list, "\n")
+        for child in child_node_list:
+            #print("child: ", child.getSimModelObject().RefId()) 		
+            selected_info = []		
+		# Every child will eventually become a modelica component. Thus we need:
+        # Name/ID - Type - & if applicable InPort and Outport from each TargePort and SourcePort
+		# The following structure (list of lists) will be used:
+		# for each node: [RefId,Type,[list of InPorts],[list of OutPorts],[list of InOrOutPorts]] where each element of the list InPorts
+		# is a list of the form: [TargetPort,SourcePort] 
+            objectRefId = child.getSimModelObject().RefId()
+            objectType = child.ClassType()
+            selected_info.extend([objectRefId,objectType])			
+        #InPort and Outport			
+            child_node_list_2 = child.getChildList()
+            InPortlist = []  #SimDistributionPort_HotWaterFlowPort_Water_Out
+            OutPortlist = [] #SimDistributionPort_HotWaterFlowPort_Water_In			
+            for child_2lvl in child_node_list_2:
+    # We need to go 1 level deeper to get Source- and TargetPort. Both are in class SimConnection_HotWaterFlow_Default (Port[0])
+    # Attribute TargetPort = OutPort.RefId() (Water_In) and Attribute SourcePort = InPort.RefId() (Water_Out)
+	#EXAMPLE:
+	#ID0LMbjl8XfFaui1YxJs_D6D
+	#InPort: ID3zs117w1n5lwSRidPGhKht (Water_Out)
+	#	SourcePort: ID3zs117w1n5lwSRidPGhKht
+    #	TargetPort: ID13UhRNCnX1rAwEBMyxljsq
+	#OutPort: ID1a5meudDCPOBQD1vrPCFy (Water_In)
+    #	SourcePort: ID3mJZcz594IfdQpbAKBQkZ
+	#	TargetPort: ID1a5meudDCPOBQD1vrPCFy
+    #
+    # In case the calss is HotWaterFlowPort_Water_InOrOut
+	# The above rule (compare attribute SourcePort and TargetPort with object RefId)
+	# helps to know whether the object is an InPort or OutPort.
+    # We will later associate InPort with OutPorts.
+	# InPort.RefId() = InPort.SourcePort = OutPort.SourcePort  --> Alternative -->  InPort.TargetPort = OutPort.TargetPort = OutPort.RefId()
+    # we use -> TargetPort	
+                if child_2lvl.isClassType("SimDistributionPort_HotWaterFlowPort_Water_In"):				
+                    Port = child_2lvl.getChildList()
+                    obj = Port[0].getSimModelObject()
+                    OutPortlist.append(child_2lvl.getSimModelObject().RefId())
+                if child_2lvl.isClassType("SimDistributionPort_HotWaterFlowPort_Water_Out"):		
+                    Port = child_2lvl.getChildList()
+                    obj = Port[0].getSimModelObject()
+                    class_name = getattr(obj,"TargetPort")				
+                    instance = class_name()
+                    Port_Id = class_name().getValue()					
+                    InPortlist.append(Port_Id)
+                if child_2lvl.isClassType("SimDistributionPort_HotWaterFlowPort_Water_InOrOut"):				
+                    Port = child_2lvl.getChildList()
+                    obj = Port[0].getSimModelObject()				
+                    class_name = getattr(obj,"TargetPort")				
+                    instance = class_name()
+                    TargetPort_Id = class_name().getValue()
+                    class_name = getattr(obj,"SourcePort")				
+                    instance = class_name()
+                    SourcePort_Id = class_name().getValue()
+		# If TargetPort_Id = child_2lvl.getSimModelObject().RefId() we have an OutPort (Water_In class)
+		# we append SourcePort_Id to OutPortlist otherwise
+        # we have an InPort (Water_Out class) so we append SourcePort_Id to the InPort list!		
+                    if child_2lvl.getSimModelObject().RefId() == TargetPort_Id:
+                        OutPortlist.append(TargetPort_Id)
+                    else:
+                        InPortlist.append(TargetPort_Id)					
+			# Finish reading second lvl childs:  add list of OutPort and InPort into selected info
+            selected_info.extend([InPortlist,OutPortlist])			
+		     # Finisch with HVAC child. append structured information of the node into global list.
+            Structured_HVAC.append(selected_info)
+		
+G=nx.DiGraph()
+listof_RefId  = [elem[0] for elem in Structured_HVAC]
+listof_type   = [elem[1] for elem in Structured_HVAC]
+listof_InPorts  = [elem[2] for elem in Structured_HVAC]
+listof_OutPorts = [elem[3] for elem in Structured_HVAC]
+
+G.add_nodes_from(listof_RefId)	
+for node in Structured_HVAC:
+    G.node[node[0]]['Text'] = node[0]
+    G.node[node[0]]['Description'] = node[1]
+	
+for element_in in Structured_HVAC:
+    for InPort in element_in[2]:   
+        for element_out in Structured_HVAC:
+            for OutPort in element_out[3]: 		
+               if InPort == OutPort:
+                    print("ElementIN: ", element_in[0]," with InPort: ", InPort, "found OutPort: ", OutPort, " in element ", element_out[0])			   
+                    G.add_edge(element_in[0],element_out[0])		
+	
+print("number of edges: ", G.number_of_edges(), " - number of nodes: ", G.number_of_nodes())	
+nx.write_graphml(G,"test.graphml")
+
+
+	
 print("finish")
