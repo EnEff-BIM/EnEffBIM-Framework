a917220f4c40523c6af10519da2ed351135bdd1d
Carles Ribas Tugores <c.ribastugores@udk-berlin.de>
2016-10-26 14:07:46 +0200

    update libSimModel.py

--- CoTeTo/CoTeTo_DataAPI/libSimModel.py
+++ CoTeTo/CoTeTo_DataAPI/libSimModel.py
@@ -11,8 +11,21 @@ helptxt = """
 Help yourself"""
 
 def fetchData(uriList, systemCfg, generatorCfg, logger):
-    from mapapi.MapClasses import MapProject    
+    from mapapi.MapClasses import MapProject
+    import os    
     if not uriList:
         logger.critical('libSimModel - no files specified!')
         raise Exception('No files specified!')
-    return {'MapProject': MapProject(uriList[0])}
+
+    rootPath = os.path.dirname(__file__)
+    default_path = rootPath[:rootPath.rfind("EnEffBIM-Framework")]
+    modulePath = os.path.join(default_path, "EnEffBIM-Framework\\SimModel_Python_API"
+                              "\\simmodel_swig\\Release")
+
+    zoneFile_path = uriList[1]
+    hvacFile_path = uriList[0]
+    print("zoneFile_path: ", uriList[0], "\n hvacFile_path", uriList[1],"\n")    	
+    pathList = (zoneFile_path, hvacFile_path)
+    mappingFile_path = os.path.join(modulePath + "\\mapping_rule\\mapping_rule_xml\\AixLib_v2.6.xml")
+
+    return {'MapData': MapProject(pathList,mappingFile_path)}
